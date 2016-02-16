af05a9024de6feb4f65cc6722b9edbf30591d1dd
277856057 <277856057@qq.com>
2016-02-16 14:50:16 +0800

    add

---
+++ accumulation/case/yanGeMoShi/1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by qiujinlong on 2016/2/14.
+ */
+"use strict";
+function show() {
+    a = 12;
+}
+show();
+alert(a);
---
+++ accumulation/case/yanGeMoShi/2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qiujinlong on 2016/2/14.
+ */
+function show2() {
+    b = 12;
+}
+show2();
+alert(b);
